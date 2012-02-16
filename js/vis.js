c705a197b6eb5fbed39bb60413649ec81dbcf1b0
Jim V <vlandham@gmail.com>
2012-02-16 16:47:12 -0600

    use d3.scales

--- js/vis.js
+++ js/vis.js
@@ -2,22 +2,32 @@ $(document).ready(function() {
   var w = 600,
       h = 600;
 
+  var radius = d3.scale.linear().range([14, 30]);
+  var y = d3.scale.linear().range([40, 560]);
+  var x = d3.scale.linear().range([40, 560]);
+
   var vis = d3.select("#vis").append("svg")
     .attr("id", "svg-vis")
     .attr("width", w)
     .attr("height", h);
 
-  render_vis = function(csv) {
+  function render_vis(csv) {
+    radius.domain([0, d3.max(csv, function(d) { return d.r; })]);
+    y.domain([0, d3.max(csv, function(d) { return d.y; })]);
+    x.domain([0, csv.length]);
+
+
     vis.selectAll("circle")
     .data(csv).enter()
     .append("circle")
-    .attr("r",function(d) { return d.r * 3; })
-    .attr("cy", function(d) { return (d.y+1) * 3; })
-    .attr("cx", function(d,i) { return (i + 1) * 40; })
-    .attr("title", function(d) { return "Im a circle! " + d.x; })
+    .attr("r", function(d) { return radius(d.r);})
+    .attr("cy", function(d) { return y(d.y); })
+    .attr("cx", function(d,i) { return x(i); })
     .attr("fill", "steelblue")
-    .call(d3.behavior.drag().on("drag", move).on("dragstart", dragstart).on("dragend", dragend));
-  }
+    .call(d3.behavior.drag().on("drag", move)
+        .on("dragstart", dragstart)
+        .on("dragend", dragend));
+  };
 
   function dragstart() {
     d3.select(this).attr("fill", "red");
@@ -26,7 +36,6 @@ $(document).ready(function() {
   function dragend() {
     d3.select(this).attr("fill", "steelblue");
   };
-
 
   function move() {
     var dragTarget = d3.select(this);
